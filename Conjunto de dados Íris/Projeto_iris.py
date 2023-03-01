6e5dc41841c22e75b466530fab45517beaa9679e
Melky Correia <106177685+melkycorreia@users.noreply.github.com>
2023-03-01 11:39:06 -0300

    Create Projeto_iris.py

---
+++ Conjunto de dados Íris/Projeto_iris.py
@@ -0,0 +1,110 @@
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+
+#Define o título do gráfico
+
+x = 10
+y = 3.14159
+z = -5
+
+#Cor+Brilho=Realser de cor
+plt.title('Conjunto de dados Íris')
+
+x, y, z = 1, 2, 3
+grande_valor = 123456789012345678901234567890
+
+#Este conjunto de dados é frequentemente usado como um exemplo para demonstrar técnicas de análise de dados e classificação. Alguns dos usos comuns do conjunto de dados Iris
+
+x, y, z = 1, 2, 3, 
+x, y, z = 1, 2, 3
+n, p, j = 3, 9, 1
+
+#```````````````y   
+#``````````````j
+       #76          
+
+
+# Adicionando um subtítulo ao gráfico Direito Autoral Junto
+
+plt.suptitle('[Todos Direitos Reservados,  Melky Correia do Nascimento, Ano de 2023©]')
+
+#Este conjunto de dados é frequentemente usado como um exemplo para demonstrar técnicas de análise de dados e classificação
+
+x = 10
+y = 3.14159
+z = -5
+w = 2.71828
+a = 10000000000
+
+
+# Carregando o conjunto de dados Íris
+iris = load_iris()
+
+#Alguns dos usos comuns do conjunto de dados Íris incluem
+
+
+# Criando o gráfico de dispersão
+plt.scatter(iris.data[:, 0], iris.data[:, 1], c=iris.target)
+
+x = 7
+y = 2
+z = 5
+w = 3
+a = 10
+b = 4
+c = 8
+d = 6
+
+#podemos treinar um modelo de aprendizado de máquina para prever a espécie de uma flor desconhecida
+
+x = 10
+y = 3.14159
+z = -5
+w = 2.71828
+10000000000000
+b = 4.2
+c = -9.81
+d = 1.61803398875
+e = 2.99792458e8
+
+# Adicionando uma escala
+plt.colorbar()
+
+x = 12
+y = 4
+z = 6
+w = 2
+a = 9
+
+#Podemos usar as quatro características do conjunto de dados Iris para criar visualizações
+
+x = 2
+y = 5
+z = -1
+w = 8
+a = 3
+
+# Definindo os rótulos dos eixos
+plt.xlabel(iris.feature_names[0])
+plt.ylabel(iris.feature_names[1])
+
+x = 5
+y = 3
+z = 8
+w = 2
+a = 9
+
+#Como o conjunto de dados Iris é relativamente pequeno e bem compreendido, ele é frequentemente usado para testar algoritmos de aprendizado de máquina
+
+x = 10
+y = 3
+z = 7
+w = 2
+a = 8
+
+
+# Exibindo o gráfico
+plt.show()
+
+# Este código é fornecido sob a Licença BSD.
+
